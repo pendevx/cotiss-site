877bec523b4c8165ba37f9acd7828e44e92050f9
Pendevx <pendevx@gmail.com>
2023-01-13 19:33:00 +1300

    removed debugging stuff

--- pages/api/get-api-keys.js
+++ pages/api/get-api-keys.js
@@ -9,9 +9,6 @@ export default function getApiKeys(req, res) {
         res.status(400);
         return;
     }
-
-    console.log(process.env.DDB_Quotes_AKI);
-    console.log(process.env.DDB_Quotes_SAK);
     
     res.status(200).json({
         accessKeyId: process.env.DDB_Quotes_AKI,
